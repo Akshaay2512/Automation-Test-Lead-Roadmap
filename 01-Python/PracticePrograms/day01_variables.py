fc19f5dea70d2a122dace1ec85b17a33161edd85
Akshaay <kiranakshay2512@gmail.com>
2026-07-19 08:22:40 +0530

    Added Day 01 Python Variables Program

---
+++ 01-Python/PracticePrograms/day01_variables.py
@@ -0,0 +1,11 @@
+print("=" * 50)
+print("Python Automation Learning Journey")
+print("=" * 50)
+
+name = "Akshaay Kiran"
+experience = 8
+goal = "Senior Automation Test Lead"
+
+print(f"Name       : {name}")
+print(f"Experience : {experience} Years")
+print(f"Goal       : {goal}")
